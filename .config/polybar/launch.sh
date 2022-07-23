4ff649a66004fe90689c081e56d33e3157e5cd87
mahtjarohst <matthewrust21@gmail.com>
2022-07-23 18:32:13 -0500

    updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload main &
-done
-
-echo "Bars launched..."
+if type "xrandr"; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+              MONITOR=$m polybar --reload main &
+                done
+            else
+                  polybar --reload main &
+fi
